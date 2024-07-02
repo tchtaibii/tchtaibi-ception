7346ae4d9ca62e14cbc051c8eed56359bb865550
mac <mac@MacBook-Pro-2.local>
2024-07-03 00:19:08 +0100

    fix dependecies

--- p3/run.sh
+++ p3/run.sh
@@ -1,14 +1,18 @@
+if [ "$(id -u)" != "0" ]; then
+    echo "This script needs to be run with sudo."
+    exit 1
+fi
 sh script/setup.sh
 sleep 1
 tput setaf 2 ; echo "Change the service of Argo-cd to Node Port." 
 kubectl patch service argocd-server -n argocd --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"}]' > /dev/null
 sleep 2
 tput setaf 2 ; echo "apply deployment ..."
-kubectl apply -f conf/deployment.yml > /dev/null
+kubectl apply -f confs/deployment.yml > /dev/null
 tput setaf 2 ; echo "apply argo deployment ..."
-kubectl apply -f conf/argo.yml > /dev/null
+kubectl apply -f confs/argo.yml > /dev/null
 tput setaf 2 ; echo "apply service ..."
-kubectl apply -f conf/service.yml > /dev/null 
+kubectl apply -f confs/service.yml > /dev/null 
 sleep 30
 tput setaf 2 ; echo "Forwarding connection from your local machine to the Argo CD server running in the Kubernetes cluster. "
 kubectl port-forward -n argocd svc/argocd-server 9000:443 > /dev/null
